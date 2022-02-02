cb07f23d6e4067a3cd603c714296a34c2470c19c
MantaRaRa <97355805+MantaRaRa@users.noreply.github.com>
2022-02-01 19:59:32 -0500

    Add files via upload

---
+++ Tests/DSD.BLTest/CustomerTest.cs
@@ -0,0 +1,123 @@
+﻿using DSD.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DSD.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "EverSong",
+                LastName = "Berry"
+            };
+            string expected = "Berry, EverSong";
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                LastName = "Berry"
+            };
+            string expected = "Berry";
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "EverSong"
+            };
+            string expected = "EverSong";
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void StaticTest()
+        {
+            //-- Arrange
+            var c1 = new Customer();
+            c1.FirstName = "EverSong";
+            Customer.InstanceCount += 1;
+
+            var c2 = new Customer();
+            c2.FirstName = "Everleigh";
+            Customer.InstanceCount += 1;
+
+            var c3 = new Customer();
+            c3.FirstName = "Evenge";
+            Customer.InstanceCount += 1;
+
+            //-- Act
+
+            //-- Assert
+            Assert.AreEqual(3, Customer.InstanceCount);
+        }
+
+        [TestMethod]
+        public void ValidateValid()
+        {
+            //-- Arrange
+            var customer = new Customer
+            {
+                LastName = "Berry",
+                EmailAddress = "berry@gmail.com"
+            };
+
+            var expected = true;
+
+            //-- Act
+            var actual = customer.Validate();
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidateMissingLastName()
+        {
+            //-- Arrange
+            var customer = new Customer
+            {
+                EmailAddress = "berry@gmail.com"
+            };
+
+            var expected = false;
+
+            //-- Act
+            var actual = customer.Validate();
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
+
